bcea5bfd89a3ddc0a3a1378b1b25e19085c7ec2e
Arini11 <elarnaumc@gmail.com>
2022-09-06 11:01:20 +0200

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { DishesComponent } from './dishes/dishes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishesComponent
   ],
   imports: [
     BrowserModule
