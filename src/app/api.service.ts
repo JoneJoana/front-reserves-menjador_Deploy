4c0314d1699123e61b61ed59b3221ae2d98d4760
Joana <joanajanefdez@gmail.com>
2022-09-09 12:11:42 +0200

    fix initial values tables dishes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 const BASE = 'https://tch-db.herokuapp.com';
 const headers = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ "Authorization": "bearer eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2NjE5NjM4NDUsImlzcyI6ImFkbWluIiwic3ViIjoicm9vdCIsImV4cCI6MTY2MjgyNzg0NX0.OuH-CkYfq6WmaBBQkd3w6aWRk77uEp5P5whuCAgyUVzooQmqgIdQlc7M-DoJxBj3seXgdtnoGmWfOkx4eQ7aBQ" })
 };
 
 @Injectable({
@@ -18,3 +18,24 @@ export class OrdersService {
     return this.http.get(BASE+"/api/orders", headers);
   }
 }
+
+/* export class DishesService {
+
+  constructor(private http: HttpClient) { }
+
+  getDishes(): Observable<any> {
+    return this.http.get(BASE+"/api/dishes", headers);
+  }
+
+}
+
+export class CategoriesService {
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<any> {
+    return this.http.get(BASE+"/api/categories", headers);
+  }
+
+}
+ */
--- src/app/dishes/dishes.component.ts
+++ src/app/dishes/dishes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrdersService } from '../api.service';
 
 @Component({
   selector: 'app-dishes',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DishesComponent implements OnInit {
 
-  constructor() { }
+  admin: boolean = true;
+  dishes: Dish[] = [];
+  retrievedImage: any;
+
+  constructor() { } //private api:DishesService, private router: Router
 
   ngOnInit(): void {
+    //this.loadDishes()
+    const dish1: Dish = {
+      name: 'arroz',
+      popularity: 2,
+      status: true
+    }
+    const dish2: Dish = {
+      name: 'salmon',
+      popularity: 5,
+      status: true
+    }
+    const dish3: Dish = {
+      name: 'pollo al horno',
+      popularity: 3,
+      status: false
+    }
+    this.dishes.push(dish1);
+    this.dishes.push(dish2);
+    this.dishes.push(dish3);
   }
 
+  /* loadDishes() {
+    this.api.getDishes().subscribe(
+      response => {
+        this.dishes = response
+        console.log(response)
+        if(this.dishes == null) console.log("undef")
+        //this.retrievedImage = ;
+      }
+    )
+  } */
+
+}
+
+export interface Dish{
+  name: string,
+  popularity: number,
+  status: boolean
 }
