484cad7faf6ed7c49044af67ee067c3f2c248d5b
Joana <joanajanefdez@gmail.com>
2022-09-09 20:33:22 +0200

    implement methods to acces api - with errors

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Dish } from './dishes/dishes.component';
 
 const BASE = 'https://tch-db.herokuapp.com';
 const headers = {
@@ -31,7 +32,20 @@ export class OrdersService {
     return this.http.get(BASE+"/api/dishes", headers);
   }
 
+  postDish(newDish: Dish): Observable<any> {
+    return this.http.post(BASE+"/api/dishes/add", newDish, headers);
+  }
+
+  putDish(dish: Dish): Observable<any> {
+    return this.http.put(BASE+"/api/dishes/update", dish, headers);
+  }
+
+  deleteDish(id: Number): Observable<any> {
+    return this.http.delete(BASE+"/api/dishes/delete/"+id, headers);
+  }
+
 }
+
 
 @Injectable({
   providedIn: 'root'
--- src/app/dishes/dishes.component.ts
+++ src/app/dishes/dishes.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DishesService, OrdersService } from '../api.service';
-declare var $: any;
 
 @Component({
   selector: 'app-dishes',
@@ -11,16 +10,21 @@ declare var $: any;
 export class DishesComponent implements OnInit  {
 
   admin: boolean = true;
-  dishes: any; //Dish[] = [];
+  dishes: any; //Dish[] = []; en el cas dutilitzar les dades const dish1 etc
   retrievedImage: any;
+  newDish: Dish = {
+    name: '',
+    popularity: 0,
+    status: false
+  }
+  addDish = false;
 
-  constructor(private api:DishesService, private router: Router) { } //private api:DishesService, private router: Router
+  constructor(private api:DishesService, private router: Router) { }
 
   ngOnInit(): void {
     this.loadDishes()
-    $("select").niceSelect()
 
-    const dish1 = {
+    /* const dish1 = {
       name: 'arroz',
       popularity: 2,
       status: true
@@ -37,15 +41,13 @@ export class DishesComponent implements OnInit  {
     }
     this.dishes.push(dish1);
     this.dishes.push(dish2);
-    this.dishes.push(dish3);
+    this.dishes.push(dish3); */
   }
 
-  loadDishes() {
+  loadDishes() {  //esta dando warning en console :ERROR Error: Uncaught (in promise): InvalidStateError: An attempt was made to use an object that is not, or is no longer, usable
     this.api.getDishes().subscribe(
       response => {
         this.dishes = response
-        console.log(response)
-        if(this.dishes == null) console.log("undef")
         //this.retrievedImage = ;
       },
       error => {
@@ -55,7 +57,88 @@ export class DishesComponent implements OnInit  {
   }
 
   add(){
+    this.addDish = true;
+  }
 
+  save(){
+    if(this.newDish.name != ''){
+      this.api.postDish(this.newDish).subscribe(
+        response => {
+          this.dishes = response
+          //this.retrievedImage = ;
+          this.loadDishes();
+          this.clearNewDish();
+          this.addDish = false;
+          if(response==null){
+            alert('No sha guardat el plat. Nom repetit');
+          }
+        },
+        error => {
+          console.log("ERROR REQUEST" + error.message)
+        }
+      )
+    }else{
+      console.log("El nom esta buit. No sha guardat.")
+    }
+
+  }
+
+  cancel(){
+    this.addDish = false;
+    this.clearNewDish();
+  }
+
+  clearNewDish(){
+    this.newDish = {
+      name: '',
+      popularity: 0,
+      status: false
+    }
+  }
+
+
+  /*DA ERROR: (no modifica bbdd)
+    XHROPTIONShttps://tch-db.herokuapp.com/api/dishes/delete/121
+    CORS Missing Allow Origin
+
+    Solicitud desde otro origen bloqueada: la política de mismo origen impide leer el recurso remoto en https://tch-db.herokuapp.com/api/dishes/delete/121 (razón: falta la cabecera CORS 'Access-Control-Allow-Origin'). Código de estado: 403.
+
+    Solicitud de origen cruzado bloqueada: La misma política de origen no permite la lectura de recursos remotos en https://tch-db.herokuapp.com/api/dishes/delete/121. (Razón: Solicitud CORS sin éxito). Código de estado: (null).
+
+    ERROR REQUESTHttp failure response for https://tch-db.herokuapp.com/api/dishes/delete/121: 0 Unknown Error dishes.component.ts:109:16 */
+  delete(id: number){
+    this.api.deleteDish(id).subscribe(
+      response => {
+        this.dishes = response
+        //this.retrievedImage = ;
+        this.loadDishes();
+        this.clearNewDish();
+        this.addDish = false;
+      },
+      error => {
+        console.log("ERROR REQUEST" + error.message)
+      }
+    )
+  }
+
+  update(dish: any){//fallo como delete (no modifica bbdd)
+    if(dish.name != ''){
+      this.api.putDish(dish).subscribe(
+        response => {
+          this.dishes = response
+          //this.retrievedImage = ;
+          this.loadDishes();
+          if(response==null){
+            alert('No sha guardat el plat. Nom repetit');
+          }
+        },
+        error => {
+          console.log("ERROR REQUEST" + error.message)
+        }
+      )
+    }else{
+      console.log("El nom esta buit. No sha guardat.")
+    }
   }
 
 }
