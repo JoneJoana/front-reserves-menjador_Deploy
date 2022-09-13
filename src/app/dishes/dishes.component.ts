eaba736bd8d3cdca312452fd397526a2d1370524
Joana <joanajanefdez@gmail.com>
2022-09-13 16:13:34 +0200

    con fallos en updateDish i checks ctgs - pendent

--- src/app/dishes/dishes.component.ts
+++ src/app/dishes/dishes.component.ts
@@ -1,18 +1,23 @@
+import { NgForOf } from '@angular/common';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { DishesService, OrdersService } from '../api.service';
+import {
+  CategoriesService,
+  DishesService,
+  OrdersService,
+} from '../api.service';
 //declare var $: any;
-
 
 @Component({
   selector: 'app-dishes',
   templateUrl: './dishes.component.html',
-  styleUrls: ['./dishes.component.css']
+  styleUrls: ['./dishes.component.css'],
 })
-export class DishesComponent implements OnInit  {
-
+export class DishesComponent implements OnInit {
   admin: boolean = true;
   dishes: any; //agafant dades de la bbdd // Dish[] en el cas dutilitzar les dades const dish1 etc
+  categories: any;
+  ctgName: any;
   retrievedImage: any;
   addDish = false;
 
@@ -21,20 +26,50 @@ export class DishesComponent implements OnInit  {
     image: '',
     popularity: 0,
     status: false,
-    categories: []
-  }
+    categories: [],
+  };
 
-  //sabiendo que hay 3 platos
-  visibilityImg: boolean[] = [true,true,true,true,true,true,true,true,true,true,true,true,true];
-  visibilityFormFile: boolean[] = [false,false,false,false,false,false,false,false,false,false,false,false,false];
+  //sabiendo que hay 13 platos
+  visibilityImg: boolean[] = [
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+  ];
+  visibilityFormFile: boolean[] = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
 
-  possibleCategories = ['Pescado','carne','arroz','shushi','primero','segundo','postre']
-
-
-  constructor(private api:DishesService, private router: Router) { }
+  constructor(
+    private api: DishesService,
+    private api2: CategoriesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loadDishes();
+    this.loadCategories();
     /* setTimeout(() => {
       $("select").niceSelect()
     },500) */
@@ -63,110 +98,117 @@ export class DishesComponent implements OnInit  {
     this.dishes.push(dish1);
     this.dishes.push(dish2);
     this.dishes.push(dish3); */
-
   }
 
-  changeVisibility(indexDish: number){
+  changeVisibility(indexDish: number) {
     this.visibilityFormFile[indexDish] = true;
     this.visibilityImg[indexDish] = false;
-
   }
 
   loadDishes() {
     this.api.getDishes().subscribe(
-      response => {
-        this.dishes = response
+      (response) => {
+        this.dishes = response;
         console.log(this.dishes);
         //this.retrievedImage = ;
       },
-      error => {
-        console.log("ERROR REQUEST")
+      (error) => {
+        console.log('ERROR REQUEST');
       }
-    )
+    );
   }
 
-  add(){
+  loadCategories() {
+    this.api2.getCategories().subscribe(
+      (response) => {
+        this.categories = response;
+        console.log(this.categories);
+        /* for (let i = 0; i < this.categories.length; i++) {
+          console.log(this.categories[i].name);
+          this.ctgName.push(this.categories[i].getName());
+        } */
+      },
+      (error) => {
+        console.log('ERROR REQUEST');
+      }
+    );
+  }
+
+
+
+  add() {
     this.addDish = true;
   }
 
-  save(){
-    if(this.newDish.name != ''){
+  save() {
+    if (this.newDish.name != '') {
       this.api.postDish(this.newDish).subscribe(
-        response => {
-          this.dishes = response
+        (response) => {
+          this.dishes = response;
           //this.retrievedImage = ;
           this.loadDishes();
           this.clearNewDish();
           this.addDish = false;
-          if(response==null){
+          if (response == null) {
             alert('No sha guardat el plat. Nom repetit');
           }
         },
-        error => {
-          console.log("ERROR REQUEST" + error.message)
+        (error) => {
+          console.log('ERROR REQUEST' + error.message);
         }
-      )
-    }else{
-      console.log("El nom esta buit. No sha guardat.")
+      );
+    } else {
+      console.log('El nom esta buit. No sha guardat.');
     }
-
   }
 
-  cancel(){
+  cancel() {
     this.addDish = false;
     this.clearNewDish();
   }
 
-  clearNewDish(){
+  clearNewDish() {
     this.newDish = {
       name: '',
       image: '',
       popularity: 0,
       status: false,
-      categories: []
-    }
+      categories: [],
+    };
   }
 
-  delete(id: number){
+  delete(id: number) {
     this.api.deleteDish(id).subscribe(
-      response => {
-        this.dishes = response
-        //this.retrievedImage = ;
-        console.log(response + 'hola')
+      (response) => {
+        this.dishes = response;
         this.loadDishes();
       },
-      error => {
-        console.log("ERROR REQUEST" + error.message)
+      (error) => {
+        console.log('ERROR REQUEST' + error.message);
       }
-    )
+    );
   }
 
-  update(dish: any){
-    if(dish.name != ''){
-      this.api.putDish(dish).subscribe(
-        response => {
-          this.dishes = response
+  update(id: number) {
+
+      this.api.putDish(this.dishes[id]).subscribe(
+        (response) => {
+          this.dishes = response;
           //this.retrievedImage = ;
           this.loadDishes();
-          if(response==null){
-            alert('No sha guardat el plat. Nom repetit');
-          }
         },
-        error => {
-          console.log("ERROR REQUEST" + error.message)
+        (error) => {
+          console.log('ERROR REQUEST' + error.message);
         }
-      )
-    }else{
-      console.log("El nom esta buit. No sha guardat.")
-    }
-  }
+      );
 
+  }
 }
 
-export interface Dish{
-  name: string,
-  image: any,
-  popularity: number,
-  status: boolean,
+export interface Dish {
+  name: string;
+  image: any;
+  popularity: number;
+  status: boolean;
   categories: string[];
 }
