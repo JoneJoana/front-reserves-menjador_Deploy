c1c8ceb05f96dcf7e899a4dfe8a1077b89b692dd
Joana <joanajanefdez@gmail.com>
2022-09-18 01:58:30 +0200

    format + neteja buscar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,150 +4,112 @@ import { CategoriesService, DishesService } from '../api.service';
 import { MAIN_CATEGORIES } from '../Constants';
 declare var $: any;
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  dishes:any
-  categories:any
-  isotope = false
+  dishes: any;
+  categories: any;
+  isotope = false;
   buscar: string = '';
   // quick search regex
-  qsRegex:any = undefined;
+  qsRegex: any = undefined;
 
-  constructor(private api:DishesService, private api2: CategoriesService) { }
+  constructor(private api: DishesService, private api2: CategoriesService) {}
 
   ngOnInit(): void {
-    this.getDishes()
+    this.getDishes();
     this.loadCategories();
-
-
   }
 
   getDishes() {
     this.api.getDishes().subscribe(
-      response => {
-        this.dishes = response
+      (response) => {
+        this.dishes = response;
       },
-      error => {
-        console.log("ERROR REQUEST:\n "+error.message)
-      }
-    )
-  }
-
-  loadCategories() {
-    this.api2.getCategories().subscribe(
-      response => {
-        this.categories = response;
-
-        // Posar les categories principals al principi i la resta al final
-        var mainCat = Array()
-        var extraCat = Array()
-        this.categories.forEach((c:any) => {
-          if(MAIN_CATEGORIES.includes(c.id))
-            mainCat.push(c)
-          else
-            extraCat.push(c)
-        });
-        this.categories = mainCat.concat(extraCat)
-
-      },
-      error => {
-        console.log("ERROR REQUEST:\n "+error.message);
+      (error) => {
+        console.log('ERROR REQUEST:\n ' + error.message);
       }
     );
   }
 
-  doIsotopeMagic(e:any){
-/*
-    // Inicialitzar isotope un cop cada vegada que es carrega el component Home
-    if(!this.isotope) {
-      console.log("ISOTOPE INICIALITZAT")
-      this.isotope = true
-    // Inicialitzar
-    var iso = new Isotope( '.grid', {
-      itemSelector: '.all',
-      percentPosition: false,
-      masonry: {
-        columnWidth: ".all"
-      }
-    });
+  loadCategories() {
+    this.api2.getCategories().subscribe(
+      (response) => {
+        this.categories = response;
 
+        // Posar les categories principals al principi i la resta al final
+        var mainCat = Array();
+        var extraCat = Array();
+        this.categories.forEach((c: any) => {
+          if (MAIN_CATEGORIES.includes(c.id)) mainCat.push(c);
+          else extraCat.push(c);
+        });
+        this.categories = mainCat.concat(extraCat);
+      },
+      (error) => {
+        console.log('ERROR REQUEST:\n ' + error.message);
+      }
+    );
+  }
+
+  doIsotopeMagic(e: any) {
+    // Inicialitzar isotope un cop cada vegada que es carrega el component Home
+    if (!this.isotope) {
+      console.log('ISOTOPE INICIALITZAT');
+      this.isotope = true;
+      // Inicialitzar
+      var iso = new Isotope('.grid', {
+        itemSelector: '.all',
+        percentPosition: false,
+        masonry: {
+          columnWidth: '.all',
+        },
+      });
 
       // Carregar listeners botons
       var filtersElem = document.querySelector('.filters_menu')!;
-      filtersElem.addEventListener( 'click', ( event ) => {
+      filtersElem.addEventListener('click', (event) => {
         // Obtenir element per filtrar
         var filterValue = (event.target as any).getAttribute('id');
 
-
-          iso.arrange({ filter: filterValue });
+        iso.arrange({ filter: filterValue });
       });
-
     }
 
     // Actualitzar estil boto actiu
     $('.filters_menu li').removeClass('active');
     $(e.target).addClass('active');
-    */
-  }
-/*
-  onSearchKeyUp(e:any) {
-        // Inicialitzar
-        var iso = new Isotope( '.grid', {
-          itemSelector: '.all',
-          layoutMode: 'fitRows',
 
-        });
-    //var qsRegex;
-    // Passar filtre buscador
-    iso.arrange({ filter: function() {
-      var nom = $(this).find('.nomPlat').text();
-      var valor = e.target.value
-      //new RegExp( $quicksearch.val(), 'gi' );
-      //if(nom.includes(valor))
-      var qsRegex = new RegExp( valor, 'gi' )
-      console.log(qsRegex ? nom.match(qsRegex) : true)
-      return qsRegex ? nom.match(qsRegex) : true;
-    }});
-    //this.qsRegex = new RegExp( this.buscar, 'gi' );
-    //$grid.isotope();
+    //neteja camp busqueda al clicar una categoria
+    this.buscar = '';
   }
 
-*/
-
-  onSearchKeyUp(e:any) {
-    var iso = new Isotope( '.grid', {
+  onSearchKeyUp(e: any) {
+    var iso = new Isotope('.grid', {
       itemSelector: '.all',
       layoutMode: 'fitRows',
-      filter: function() {
+      filter: function () {
         //console.log(qsRegex ? $(this).text().match( qsRegex ) : true)
         var nom = $(this).find('.nomPlat').text();
-        var valor = e.target.value
-        var qsRegex = new RegExp( valor, 'gi' )
-        console.log("nom: "+nom)
-        console.log("valor: "+valor)
-        console.log("qsRegex: "+qsRegex)
-        console.log(qsRegex ? nom.match(qsRegex) : true)
-        console.log("-------------------")
+        var valor = e.target.value;
+        var qsRegex = new RegExp(valor, 'gi');
+        console.log('nom: ' + nom);
+        console.log('valor: ' + valor);
+        console.log('qsRegex: ' + qsRegex);
+        console.log(qsRegex ? nom.match(qsRegex) : true);
+        console.log('-------------------');
         return qsRegex ? nom.match(qsRegex) : true;
-      }
+      },
     });
 
-    if(this.buscar=="")iso.destroy()
+    if (this.buscar == '') iso.destroy();
   }
 
-
-
 }
 
-function isMainCategory(idCat:number):boolean {
-  return MAIN_CATEGORIES.includes(+idCat)
+function isMainCategory(idCat: number): boolean {
+  return MAIN_CATEGORIES.includes(+idCat);
 }
-
-
-
