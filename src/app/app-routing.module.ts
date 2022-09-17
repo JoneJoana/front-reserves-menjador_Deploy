7c4e816c861346496cd1fb3df927303b1b0c2d59
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-09-17 17:56:02 +0200

    Upda module and html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { DishesComponent } from './dishes/dishes.component';
 import { HomeComponent } from './home/home.component';
 import { OrdersComponent } from './orders/orders.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'dishes', component: DishesComponent },
   { path: 'orders', component: OrdersComponent },
-  { path: 'registro', component: RegisterComponent }
+  { path: 'registro', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
