c72367c94542439d0ffa1fcfbc235dfd8099d77b
Joana <joanajanefdez@gmail.com>
2022-09-18 20:00:35 +0200

    login add username, modify necessary

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form: any = {
+    username: null,
+    password: null
+  }
+  username?: string;
+  password?: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void{
+    alert('login correcto');
+  }
+
 }
+
