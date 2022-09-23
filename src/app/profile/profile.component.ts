00863806f2e936db5aa4690c1c2ed97c783b7177
Joana <joanajanefdez@gmail.com>
2022-09-23 15:29:04 +0200

    changePassword: validaciones, alerts, reestructura

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,34 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { USERNAME } from '../Constants';
 import { AuthService } from '../_services/auth.service';
 declare var swal: any;
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
-  user={
+  user = {
     username: this._route.snapshot.paramMap.get('username')!,
     email: '',
     password: '',
-    image: null
-  }
+    image: null,
+  };
 
-  newPass={
-    oldPassword: "",
-    newPassword: "",
-    newPasswordRepeat: ""
-  }
+  newPass = {
+    oldPassword: '',
+    newPassword: '',
+    newPasswordRepeat: '',
+  };
 
   selectedFile: File | null = null;
   hasBeenModified = false;
 
-
-  constructor(private api: AuthService,private _route: ActivatedRoute) { }
+  constructor(private api: AuthService, private _route: ActivatedRoute) {}
 
   ngOnInit(): void {
     //this.loadUser(window.sessionStorage.getItem(USERNAME));
@@ -47,77 +44,144 @@ export class ProfileComponent implements OnInit {
   }
 
   changeImage() {
-    $("#user").find(".uploadFile").trigger("click")
+    $('#user').find('.uploadFile').trigger('click');
     this.hasBeenModified = true;
   }
 
-  updateInfo(){
-    // CANVIAR CONTRASENYA
-    if(this.newPass.newPassword != "") {
-      //TODO: Validar contrasenya amb regular expression + popus confirmacio i error/exit
-      if(this.newPass.newPassword != this.newPass.newPasswordRepeat) {
-        swal({
-          text: "La contraseña nueva no coincide",
-          icon: "error",
-          button: false,
-          timer: 2000
-        });
-        return
-      }
-      const data = {
-        username: this.user.username,
-        password: this.newPass.newPassword
-      }
-      this.api.changePassword(data).subscribe(
-        response => {
-          swal({
-            text: "Contraseña actualizada! :)",
-            icon: "success",
-            button: false,
-            timer: 1500
-          });
-        },
-        error => {
-          swal({
-            text: "Missatge error change password",
-            icon: "error",
-            button: false,
-            timer: 1500
-          });
-        }
-      )
-      return
+  updateUser() {
+    let regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!regexMail.test(this.user.email)) {
+      swal({
+        text: 'Error al modificar el email, no es correcto',
+        icon: 'error',
+        button: false,
+        timer: 1800,
+      });
+      return;
+    } else {
+      // UPDATE USER
+      this.api
+        .updateUser(this.user.username, { email: this.user.email })
+        .subscribe(
+          (response) => {
+            swal({
+              text: 'Datos actualizados! :)',
+              icon: 'success',
+              button: false,
+              timer: 1500,
+            });
+            setTimeout(() => {
+              window.location.reload();
+            }, 1300);
+            if (this.hasBeenModified && this.selectedFile != null) {
+              this.updateUserImage(this.user.username, this.selectedFile);
+              setTimeout(() => {
+                window.location.reload();
+              }, 1800);
+            }
+          },
+          (error) => {
+            swal({
+              text: 'Error al modificar los datos. Lo sentimos, intentalo de nuevo :(',
+              icon: 'error',
+              button: false,
+              timer: 1500,
+            });
+          }
+        );
     }
-
-    // UPDATE USER
-    this.api.updateUser(this.user.username, {email: this.user.email}).subscribe(
-      response => {
-        swal({
-          text: "Datos actualizados! :)",
-          icon: "success",
-          button: false,
-          timer: 1500
-        });
-        if(this.hasBeenModified && this.selectedFile != null) {
-          this.updateUserImage(this.user.username,this.selectedFile)
-          setTimeout (() => {
-            window.location.reload();
-          }, 1800);
-        }
-      },
-      error => {
-        swal({
-          text: "Error al modificar los datos. Lo sentimos, intentalo de nuevo :(",
-          icon: "error",
-          button: false,
-          timer: 1500
-        });
-      }
-    )
   }
 
-  updateUserImage(username:string,f:any) {
-    this.api.updateUserImage(username,f).subscribe(
+  updatePassword() {
+    swal({
+      text: '¿Seguro que quieres actualizar tu contraseña?',
+      icon: 'warning',
+      buttons: [true, 'Si'],
+    }).then((okay: boolean) => {
+      if (okay) {
+        let regexNum = /\d/;
+        let regexSymbol = /[$-/:-?{-~!"^_`\[\]]/;
+
+        if (this.newPass.newPassword == '' || this.newPass.newPasswordRepeat == '') {
+          swal({
+            text: 'Hay campos en blanco',
+            icon: 'error',
+            button: false,
+            timer: 1500,
+          });
+          return;
+        }
+
+        const valid =
+          regexNum.test(this.newPass.newPassword) &&
+          regexSymbol.test(this.newPass.newPassword) && this.newPass.newPassword.length > 6;
+
+        console.log(this.newPass.newPassword);
+        console.log(this.newPass.newPasswordRepeat);
+        console.log(regexNum.test(this.newPass.newPassword));
+        console.log(regexSymbol.test(this.newPass.newPassword));
+        console.log(valid);
+
+        if (valid) {
+          if (!this.changePassword()) {
+            swal({
+              text: 'La contraseña nueva y su repetición no coinciden',
+              icon: 'error',
+              button: false,
+              timer: 1800,
+            });
+          }
+        }else{
+          swal({
+            text: 'Contraseña mínimo 6 caracteres (con un numero y un símbolo)',
+            icon: 'error',
+            button: false,
+            timer: 1500,
+          });
+          return;
+        }
+      }
+    });
+  }
+
+  private changePassword(): boolean {
+    // CANVIAR CONTRASENYA
+    let ok: boolean;
+
+    if (this.newPass.newPassword != this.newPass.newPasswordRepeat) return ok = false;
+
+    const data = {
+      username: this.user.username,
+      password: this.newPass.newPassword,
+    };
+    this.api.changePassword(data).subscribe(
+      (response) => {
+        swal({
+          text: 'Contraseña actualizada! :)',
+          icon: 'success',
+          button: false,
+          timer: 1500,
+        });
+        setTimeout (() => {
+          window.location.reload();
+        }, 1800);
+        return ok = true;
+      },
+      (error) => {
+        swal({
+          text: 'Error cambiando password',
+          icon: 'error',
+          button: false,
+          timer: 1500,
+        });
+        return ok = false;
+      }
+    );
+      return ok = true;
+  }
+
+  updateUserImage(username: string, f: any) {
+    this.api.updateUserImage(username, f).subscribe(
       (response) => {
         console.log('updatedishImage correcto');
         console.log(response);
@@ -127,34 +191,36 @@ export class ProfileComponent implements OnInit {
       }
     );
     this.selectedFile = null;
-}
+  }
 
-  onFileChanged(event:any) {
-    console.log("onFileChanged")
+  onFileChanged(event: any) {
+    console.log('onFileChanged');
     this.selectedFile = <File>event.target.files[0];
 
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
 
-      reader.onload = function (e : any) {
-        console.log($("#user").find(".preview").attr("src"))
-          $("#user").find(".preview").attr('src', e.target.result);
-      }
+      reader.onload = function (e: any) {
+        console.log($('#user').find('.preview').attr('src'));
+        $('#user').find('.preview').attr('src', e.target.result);
+      };
 
       reader.readAsDataURL(event.target.files[0]);
     }
   }
 
-  updatePassword() {
-    document.getElementById("goBackBtn")!.style.display = "block"
-    document.getElementById("infoBasica")!.style.display = "none"
-    document.getElementById("changePassword")!.style.display = "block"
+  changeViewToPass() {
+    document.getElementById('goBackBtn')!.style.display = 'block';
+    document.getElementById('infoBasica')!.style.display = 'none';
+    document.getElementById('changePassword')!.style.display = 'block';
   }
 
   goInicioPerfil() {
-    document.getElementById("goBackBtn")!.style.display = "none"
-    document.getElementById("infoBasica")!.style.display = "block"
-    document.getElementById("changePassword")!.style.display = "none"
+    document.getElementById('goBackBtn')!.style.display = 'none';
+    document.getElementById('infoBasica')!.style.display = 'block';
+    document.getElementById('changePassword')!.style.display = 'none';
+    this.newPass.oldPassword = '',
+    this.newPass.newPassword = '',
+    this.newPass.newPasswordRepeat = ''
   }
-
 }
